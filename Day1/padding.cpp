7baeb8e497f5a0331bfe6158bc7d815147be39e0
Sibtain Raza <khanbhai14063@gmail.com>
2026-04-17 18:04:20 +0530

    Padding how memory allocate of variables

---
+++ Day1/padding.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class padding{
+ 
+    char c;
+    int a;
+    char b;
+
+    // output will be 12 byte 
+    
+}; // this symbol should be other may occur error 
+int main()
+{
+    padding obj;
+    cout<<sizeof(obj)<<" ";
+    
+return 0;
+}
