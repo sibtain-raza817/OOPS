7190621f89d5f117eb2cb09739ef1bdcebeefea6
Sibtain Raza <khanbhai14063@gmail.com>
2026-05-02 19:03:50 +0530

    how to read space as chracter

--- Day8/file2.cpp
+++ Day8/file2.cpp
@@ -8,12 +8,13 @@ int main()
     fin.open("zoom.txt");  // Open the file "zoom.txt" for reading
     
     char c;  // Variable to store each character read from file
-    fin >> c;  // Read the first character from file
+    // fin >> c;  // Read the first character from file  it will not consider space it will ignor space 
+    c = fin.get();
     
     // Loop through file until end-of-file (EOF) is reached
     while(!fin.eof()){
         cout << c;  // Print current character to console
-        fin >> c;   // Read next character from file
+        c = fin.get();   // Read next character from file , it will consider space also as chracter
     }
     
     fin.close();  // Close the file to free system resources
